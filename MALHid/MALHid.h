3841cb011ddfc9b255ea2fb7200b816977d8c770
Rovolo <rovolo@s213-103-214-15.cust.tele2.se>
2012-01-13 13:26:25 +0100

    Initial Commit

---
+++ MALHid/MALHid.h
@@ -0,0 +1,13 @@
+//
+//  MALHid.h
+//  MALHid
+//
+//  Created by Rovolo on 1/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MALHid : NSObject
+
+@end
---
+++ MALHid/MALHid.m
@@ -0,0 +1,23 @@
+//
+//  MALHid.m
+//  MALHid
+//
+//  Created by Rovolo on 1/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MALHid.h"
+
+@implementation MALHid
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
